90f3557e7ef6aa2de98b62e6e3cc7a9cbebfff50
Finn Yap <hearthstonesigh@gmail.com>
2024-03-26 17:57:20 +0800

    d

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
-const { getFirestore } = require("firebase/firestore");
+const { getFirestore, doc, setDoc, getDoc } = require("firebase/firestore");
 const { initializeApp } = require("firebase/app");
 require("dotenv").config();
 
@@ -59,6 +59,7 @@ app.post("/pixel", async (req, res) => {
        try {
               const rgbValues = req.body["rgb"];
               const coordinates = req.body["coordinates"];
+              const gridData = req.body["newGridData"];
               const valuesString = rgbValues.substring(4, rgbValues.length - 1);
               const valuesArray = valuesString.split(", ");
               valuesArray.forEach((value, index) => {
@@ -76,13 +77,59 @@ app.post("/pixel", async (req, res) => {
               const r = valuesArray[0];
               const g = valuesArray[1];
               const b = valuesArray[2];
-              console.log(JSON.stringify(coordinates));
               port.write(`r${r}g${g}b${b}c${JSON.stringify(coordinates)}`, function (err) {
                      if (err) {
                             return console.log("Error on write: ", err.message);
                      }
               });
+              await setDoc(doc(db, "pixels", "pixels"), {
+                     gridData: JSON.stringify(gridData),
+              });
               res.status(200).header("Access-Control-Allow-Origin", "*").send("nice");
+       } catch (e) {
+              console.log(e);
+       }
+});
+
+app.get("/pixel", async (req, res) => {
+       try {
+              const docRef = doc(db, "pixels", "pixels");
+              const docSnap = await getDoc(docRef);
+              const gridArray = JSON.parse(docSnap.data().gridData);
+              gridArray.map((row, rowIndex) => {
+                     row.map((column, columnIndex) => {
+                            if (column.length > 0) {
+                                   const valuesString = column.substring(4, column.length - 1);
+                                   const valuesArray = valuesString.split(", ");
+                                   valuesArray.forEach((value, index) => {
+                                          if (valuesArray[index].length === 0) {
+                                                 valuesArray[index] = "000";
+                                          }
+                                          if (valuesArray[index].length === 1) {
+                                                 valuesArray[index] = "00" + valuesArray[index];
+                                          }
+                                          if (valuesArray[index].length === 2) {
+                                                 valuesArray[index] = "0" + valuesArray[index];
+                                          }
+                                   });
+
+                                   const r = valuesArray[0];
+                                   const g = valuesArray[1];
+                                   const b = valuesArray[2];
+                                   port.write(`r${r}g${g}b${b}c${JSON.stringify([rowIndex, columnIndex])}`, function (err) {
+                                          if (err) {
+                                                 return console.log("Error on write: ", err.message);
+                                          }
+                                   });
+                            }
+                     });
+              });
+
+              // gridArray.map((x, index) => {
+              //        console.log("index: " + ":" + index);
+              //        console.log(x);
+              // });
+              res.status(200).header("Access-Control-Allow-Origin", "*").send(docSnap.data().gridData);
        } catch (e) {
               console.log(e);
        }
